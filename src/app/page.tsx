b4b9a20c3f326f76f2843229af178a2235c49ea0
William Suarez Lumbantobing <william@Williams-MacBook-Air.local>
2024-06-10 02:18:09 +0700

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,290 @@
+"use client";
+
+import * as React from "react";
 import Image from "next/image";
+import Dropzone from "react-dropzone";
+import imageCompression, { Options } from "browser-image-compression";
+
+const useImageCompression = (options?: Options) => {
+  const [compressedSingleImage, setCompressedSingleImage] =
+    React.useState<File>();
+  const [compressedMultipleImages, setCompressedMultipleImages] =
+    React.useState<File[]>([]);
+
+  const [isCompressingImage, setIsCompressingImage] = React.useState(false);
+  const [isCompressingImages, setIsCompressingImages] = React.useState(false);
+
+  const compressSingleImage = React.useCallback(
+    async (imageFile: File) => {
+      setIsCompressingImage(true);
+
+      try {
+        const compressedFile = await imageCompression(imageFile, {
+          maxSizeMB: 0.2,
+          maxWidthOrHeight: 1920,
+          useWebWorker: true,
+          fileType: "image/jpeg",
+          ...options,
+        });
+
+        setCompressedSingleImage(compressedFile);
+        setIsCompressingImage(false);
+        return compressedFile;
+      } catch (error) {
+        // TODO: add toast
+        console.error("Image compression failed:", error);
+        setIsCompressingImage(false);
+      }
+    },
+    [options]
+  );
+
+  const compressMultipleImages = React.useCallback(
+    async (imageFiles: File[]) => {
+      setIsCompressingImages(true);
+
+      try {
+        const compressedFiles = await Promise.all(
+          imageFiles.map(async (file) => {
+            const compressedFile = await compressSingleImage(file);
+            return compressedFile;
+          })
+        );
+
+        const filteredCompressedFiles = compressedFiles.filter(
+          (file): file is File => file !== undefined
+        );
+
+        setCompressedMultipleImages(filteredCompressedFiles);
+        setIsCompressingImages(false);
+
+        return compressedFiles;
+      } catch (error) {
+        // TODO: add toast
+        console.error("Multiple image compression failed:", error);
+        setIsCompressingImages(false);
+        return [];
+      }
+    },
+    [compressSingleImage]
+  );
+
+  return {
+    compressedSingleImage,
+    compressedMultipleImages,
+    compressSingleImage,
+    compressMultipleImages,
+    isCompressingImage,
+    isCompressingImages,
+  };
+};
 
 export default function Home() {
+  const [imageFile, setImageFile] = React.useState<File>();
+  const [imageFiles, setImageFiles] = React.useState<File[]>([]);
+
+  const singleImageCompression = useImageCompression({
+    fileType: "image/webp",
+  });
+
+  const multipleImageCompression = useImageCompression({
+    fileType: "image/jpeg",
+  });
+
+  const handleImageUpload = React.useCallback(
+    async (event: React.ChangeEvent<HTMLInputElement>) => {
+      const imageFile = event.target.files?.[0];
+      setImageFile(imageFile);
+
+      if (imageFile) {
+        const compressedFile = await singleImageCompression.compressSingleImage(
+          imageFile
+        );
+        return compressedFile;
+      }
+    },
+    [singleImageCompression]
+  );
+
+  const handleMultipleImageUpload = React.useCallback(
+    async (acceptedFiles: File[]) => {
+      setImageFiles(acceptedFiles);
+
+      const compressedFiles =
+        await multipleImageCompression.compressMultipleImages(acceptedFiles);
+
+      return compressedFiles;
+    },
+    [multipleImageCompression]
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="bg-gray-400 min-h-dvh">
+      <div className="flex justify-center items-center h-dvh bg-gray-500">
+        <div className="bg-gray-400 p-6 space-y-6">
+          <h1 className="text-white font-medium text-xl">Upload</h1>
+
+          <div className="space-y-3">
+            <input
+              type="file"
+              className="w-full"
+              accept="image/*"
+              onChange={handleImageUpload}
             />
-          </a>
+
+            <div className="space-y-2">
+              <p className="font-medium">
+                original image size (
+                {((imageFile?.size ?? 0) / 1000).toFixed(2)} KB){" "}
+                {imageFile?.type}
+              </p>
+
+              <div className="w-96 h-48 flex justify-center items-center border border-gray-200">
+                {imageFile ? (
+                  <Image
+                    src={URL.createObjectURL(imageFile)}
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    className="h-full w-full object-cover"
+                    alt={`image file`}
+                  />
+                ) : (
+                  <p className="text-gray-200">no image file</p>
+                )}
+              </div>
+            </div>
+
+            <div className="space-y-2">
+              <p className="font-medium">
+                compressed image size (
+                {(
+                  (singleImageCompression.compressedSingleImage?.size ?? 0) /
+                  1000
+                ).toFixed(2)}{" "}
+                KB) {singleImageCompression.compressedSingleImage?.type}
+              </p>
+
+              {singleImageCompression.isCompressingImage && (
+                <p className="text-gray-200">compressing...</p>
+              )}
+
+              <div className="w-96 h-48 flex justify-center items-center border border-gray-200">
+                {singleImageCompression.compressedSingleImage && (
+                  <Image
+                    src={URL.createObjectURL(
+                      singleImageCompression.compressedSingleImage
+                    )}
+                    width={0}
+                    height={0}
+                    sizes="100vw"
+                    className="h-full w-full object-cover"
+                    alt={`compressed image file`}
+                  />
+                )}
+
+                {!singleImageCompression.isCompressingImage &&
+                  !singleImageCompression.compressedSingleImage && (
+                    <p className="text-gray-200">no image file</p>
+                  )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="flex justify-center items-center h-dvh bg-gray-600">
+        <div className="bg-gray-400 p-6 space-y-6">
+          <h1 className="text-white font-medium text-xl">Multiple Upload</h1>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="space-y-3">
+            <div className="w-96 h-32 border border-gray-200">
+              <Dropzone onDrop={handleMultipleImageUpload}>
+                {({ getRootProps, getInputProps }) => (
+                  <div
+                    className="w-full h-full flex justify-center items-center"
+                    {...getRootProps()}
+                  >
+                    <input {...getInputProps()} />
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                    <p className="text-gray-200">
+                      drag n drop or click to upload here
+                    </p>
+                  </div>
+                )}
+              </Dropzone>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <div className="space-y-2">
+              <p className="font-medium">original image files</p>
+              <div className="flex gap-2">
+                {imageFiles.map((imageFile, imageFileIndex) => (
+                  <div key={imageFileIndex} className="space-y-2">
+                    <div className="w-36 h-36 flex justify-center items-center border border-gray-200">
+                      <Image
+                        src={URL.createObjectURL(imageFile)}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="h-full w-full object-cover"
+                        alt={`image file`}
+                      />
+                    </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+                    <div className="space-y-1">
+                      <p className="italic">type: {imageFile.type}</p>
+                      <p className="italic">
+                        size: {((imageFile.size ?? 0) / 1000).toFixed(2)} KB
+                      </p>
+                    </div>
+                  </div>
+                ))}
+
+                {imageFiles.length === 0 && (
+                  <p className="text-gray-200">no image file</p>
+                )}
+              </div>
+            </div>
+
+            <div className="space-y-2">
+              <p className="font-medium">compressed image files</p>
+
+              {multipleImageCompression.isCompressingImages && (
+                <p className="text-gray-200">compressing...</p>
+              )}
+
+              <div className="flex gap-2">
+                {multipleImageCompression.compressedMultipleImages.map(
+                  (imageFile, imageFileIndex) => (
+                    <div key={imageFileIndex} className="space-y-2">
+                      <div className="w-36 h-36 flex justify-center items-center border border-gray-200">
+                        <Image
+                          src={URL.createObjectURL(imageFile)}
+                          width={0}
+                          height={0}
+                          sizes="100vw"
+                          className="h-full w-full object-cover"
+                          alt={`image file`}
+                        />
+                      </div>
+
+                      <div className="space-y-1">
+                        <p className="italic">type: {imageFile.type}</p>
+                        <p className="italic">
+                          size: {((imageFile.size ?? 0) / 1000).toFixed(2)} KB
+                        </p>
+                      </div>
+                    </div>
+                  )
+                )}
+
+                {!multipleImageCompression.isCompressingImages &&
+                  multipleImageCompression.compressedMultipleImages.length ===
+                    0 && <p className="text-gray-200">no image file</p>}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
